ce49a250e4ac60fe91673ccefcf1c104ea413ca3
temefaa <fateme.fa74@gmail.com>
2024-07-07 16:18:10 +0430

    refactor: ...props to props

--- components/articles/index.jsx
+++ components/articles/index.jsx
@@ -3,17 +3,16 @@ import style from './articles.module.css';
 import Image from 'next/image'
 
 
-export default function Articles(...props) {
+export default function Articles(props) {
 
-    const [articleList, setArticleList] = props
-
+    const { title , body } = props
 
     return (
             <div className={style.articleSection}>
                 <div className={style.articlesItems}>
-                    <h3  className={style.articleTitle}>{articleList.title}</h3>
+                    <h3  className={style.articleTitle}>{title}</h3>
                     <Image quality={100} width={700} height={300} src='/images/articleImg.jpg' alt='عکس مقاله'></Image>
-                    <p className={style.articleText}>{articleList.body}</p>
+                    <p className={style.articleText}>{body}</p>
                     <div className={style.options}>
                         <div>
                             <span>افزودن دیدگاه</span>
