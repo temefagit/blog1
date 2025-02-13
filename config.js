b580931448f2cec02dafba99fc32a13581028a7a
Fateme <fateme.fa74@gmail.com>
2025-02-13 20:07:57 +0330

    update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://blog-tobeginwith.vercel.app"
+      ? "blog-tobeginwith.vercel.app"
       : "http://localhost:3001",
 };
 
