ed1306726d76ee97d05086b3d225854f0a2d4791
temefaa <fateme.fa74@gmail.com>
2025-02-14 00:03:11 +0330

    fix: new post api call fixed

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -2,9 +2,26 @@ import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 
+const filePath = path.join(process.cwd(), 'api/db.json');
+
 export async function GET() {
-  const filePath = path.join(process.cwd(), 'api/db.json');
   const fileContents = fs.readFileSync(filePath, 'utf8');
   const data = JSON.parse(fileContents);
   return NextResponse.json(data.posts);
+}
+
+export async function POST(req) {
+  const { title, body } = await req.json();
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  const data = JSON.parse(fileContents);
+
+  const newPost = {
+    id: new Date().toISOString(),
+    title,
+    body,
+  };
+
+  data.posts.push(newPost);
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  return NextResponse.json(newPost);
 }
