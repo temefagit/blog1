d013e20f58136252788a6cd63d1dfcfce85f30f2
temefaa <fateme.fa74@gmail.com>
2025-02-13 22:10:17 +0330

    minor changes

--- models/articles/edit/index.tsx
+++ models/articles/edit/index.tsx
@@ -6,6 +6,7 @@ type UpdateArticleData = {
   body: string;
 };
 
+
 export const updateArticle = async (id: string, data: UpdateArticleData) => {
   const url = `${config.baseURL}/posts/${id}`;
   await axios.put(url, data);
