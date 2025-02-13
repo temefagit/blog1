4ed644babe5f05f30f62078bd349c6d045d4e191
temefaa <fateme.fa74@gmail.com>
2025-02-13 20:23:47 +0330

    feat: add combined server setup

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
+const express = require('express');
+const next = require('next');
 const jsonServer = require('json-server');
 const path = require('path');
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'api', 'db.json'));
-const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-// Error handling middleware
-server.use((err, req, res, next) => {
-  console.error('Server error:', err);
-  res.status(500).send('Internal Server Error');
-});
+const server = express();
 
-server.use(router);
+app.prepare().then(() => {
+  // JSON Server middleware
+  server.use('/api', jsonServer.router(path.join(__dirname, 'api', 'db.json')));
+  
+  // Next.js request handler
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
 
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+  server.listen(3000, err => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
 });
