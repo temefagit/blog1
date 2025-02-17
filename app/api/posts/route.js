ad17e6d39358eb7cba56afc4695651f6c0cfb618
temefaa <fateme.fa74@gmail.com>
2025-02-17 21:04:30 +0330

    feat: supabase config

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,27 +1,32 @@
-import { NextResponse } from 'next/server';
-import fs from 'fs';
-import path from 'path';
-
-const filePath = path.join(process.cwd(), 'api/db.json');
-
-export async function GET() {
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  const data = JSON.parse(fileContents);
-  return NextResponse.json(data.posts);
-}
+import { supabase } from "../../../lib/supabase";
 
 export async function POST(req) {
   const { title, body } = await req.json();
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  const data = JSON.parse(fileContents);
+  const { data, error } = await supabase
+    .from("posts")
+    .insert([{ title, body }]);
 
-  const newPost = {
-    id: new Date().toISOString(),
-    title,
-    body,
-  };
+  if (error) {
+    return new Response(
+      JSON.stringify({ message: "Internal Server Error", error }),
+      { status: 500 }
+    );
+  }
 
-  data.posts.push(newPost);
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-  return NextResponse.json(newPost);
+  return new Response(JSON.stringify(data), { status: 201 });
+}
+
+import { supabase } from "../../../lib/supabase";
+
+export async function GET() {
+  const { data, error } = await supabase.from("posts").select("*");
+
+  if (error) {
+    return new Response(
+      JSON.stringify({ message: "Internal Server Error", error }),
+      { status: 500 }
+    );
+  }
+
+  return new Response(JSON.stringify(data), { status: 200 });
 }
